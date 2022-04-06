1f4e7f3b4ef8506346d2feb55360d2c31b00f8ef
thiago teixeira <thiagom790@gmail.com>
2022-04-06 15:44:20 -0300

    criado custom create no dataProvider

--- src/providers/extendedProvider.ts
+++ src/providers/extendedProvider.ts
@@ -1,10 +1,24 @@
 import jsonServerProvider from "ra-data-json-server";
+import { fetchUtils } from "react-admin";
 import { serverUrl } from "./serverUrl";
 
 const provider = jsonServerProvider(serverUrl);
+const httpClient = fetchUtils.fetchJson;
 
 export const extendedProvider = {
   ...provider,
+  create: async (resource: string, params: any): Promise<any> => {
+    const url = `${serverUrl}/${resource}`;
+
+    const response = await httpClient(url, {
+      method: "POST",
+      body: JSON.stringify(params.data),
+    });
+
+    const data = JSON.parse(response.body);
+
+    return { data };
+  },
   update: async (resource: string, params: any): Promise<any> => {
     if (resource === "cards") {
       const { data: card, previousData: previusCard } = params;
